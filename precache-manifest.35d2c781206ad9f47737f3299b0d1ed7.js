2e221296ad0f4779066905184ffc194784cb5891
Rick Campbell <agentofmisfortune@gmail.com>
2019-11-09 00:04:26 -0500

    Updates

---
+++ precache-manifest.35d2c781206ad9f47737f3299b0d1ed7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db1811dd048a0b7e1d3fdc9f57ddbba8",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "b76b6a4da84a3276873f",
+    "url": "/robofriends/static/css/main.4a0f6e0b.chunk.css"
+  },
+  {
+    "revision": "bc1f55a4e173538a1e28",
+    "url": "/robofriends/static/js/2.f723d1db.chunk.js"
+  },
+  {
+    "revision": "b76b6a4da84a3276873f",
+    "url": "/robofriends/static/js/main.886970fb.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  }
+]);
