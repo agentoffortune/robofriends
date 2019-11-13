21bc163cde3232f5cfb3d5ee50d11022f5546845
Rick Campbell <agentofmisfortune@gmail.com>
2019-11-13 13:16:54 -0500

    Updates

---
+++ precache-manifest.923c427413609670a880d329020614e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19cd8f80204ff565e631979254fc287b",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "25c08ff22fd5ab5fe85e",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0a28f1c45bbb65f919d7",
+    "url": "/robofriends/static/css/main.54e24562.chunk.css"
+  },
+  {
+    "revision": "25c08ff22fd5ab5fe85e",
+    "url": "/robofriends/static/js/2.575b7fef.chunk.js"
+  },
+  {
+    "revision": "0a28f1c45bbb65f919d7",
+    "url": "/robofriends/static/js/main.dcaa09dd.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robofriends/precache-manifest.cf016e7a0dd327d29fe9a32601b94e34.js"
+  "/robofriends/precache-manifest.923c427413609670a880d329020614e3.js"
 );
 
 self.addEventListener('message', (event) => {
